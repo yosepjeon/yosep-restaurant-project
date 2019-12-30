d2768d1fe53a4265ad6deab5399eb4bf05c2d7c2
yosepjeon <enekelx1@naver.com>
2019-12-30 17:25:40 +0900

    프로젝트 시작

---
+++ src/main/java/com/yosep/restaurant/YosepSpringRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepSpringRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepSpringRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosep/restaurant/YosepSpringRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosep.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosepSpringRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
