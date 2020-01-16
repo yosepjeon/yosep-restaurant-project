186614b38e5a301ba902e9f6b9af2f701026faba
yosepjeon <enekelx1@naver.com>
2020-01-16 20:18:11 +0900

    Mock Object 학습 완료

--- src/test/java/com/yosep/restaurant/application/RestaurantServiceTest.java
+++ src/test/java/com/yosep/restaurant/application/RestaurantServiceTest.java
@@ -2,11 +2,18 @@ package com.yosep.restaurant.application;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import com.yosep.restaurant.domain.MenuItem;
 import com.yosep.restaurant.domain.MenuItemRepository;
@@ -17,23 +24,47 @@ import com.yosep.restaurant.domain.RestaurantRepositoryImpl;
 
 class RestaurantServiceTest {
 	private RestaurantService restaurantService;
+//	private RestaurantRepository restaurantRepository;
+//	private MenuItemRepository menuItemRepository;
+	@Mock
 	private RestaurantRepository restaurantRepository;
+	@Mock
 	private MenuItemRepository menuItemRepository;
 
 	// 이 테스트는 스프링의 테스트가 아니기 때문에 계속해서 에러를 뱉어낸다.
-	@Before
+	@BeforeTestExecution
 	public void setUp() {
-		restaurantRepository = new RestaurantRepositoryImpl();
-		menuItemRepository = new MenuItemRepositoryImpl();
-		restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
+		MockitoAnnotations.initMocks(this); // 현재 @Mock이 붙어있는 것들을 초기화해줌.
+		mockRestaurantRepository();
+		mockMenuItemRepository();
+//		restaurantRepository = new RestaurantRepositoryImpl();
+//		menuItemRepository = new MenuItemRepositoryImpl();
+		restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+	}
+
+	private void mockMenuItemRepository() {
+		// TODO Auto-generated method stub
+		List<MenuItem> menuItems = new ArrayList<>();
+		menuItems.add(new MenuItem("BigMac"));
+		given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+	}
+
+	private void mockRestaurantRepository() {
+		List<Restaurant> restaurants = new ArrayList<>();
+		Restaurant restaurant = new Restaurant(1004L, "Joker House", "Seoul");
+		restaurants.add(restaurant);
+
+		given(restaurantRepository.findAll()).willReturn(restaurants);
+		given(restaurantRepository.findById(1004L)).willReturn(restaurant);
 	}
 
 	@Test
 	public void getRestaurant() {
-		restaurantRepository = new RestaurantRepositoryImpl();
-		menuItemRepository = new MenuItemRepositoryImpl();
-		restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
-//		restaurantService.getRestaurant(1004L);
+//		restaurantRepository = new RestaurantRepositoryImpl();
+//		menuItemRepository = new MenuItemRepositoryImpl();
+//		restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+		setUp();
+		// restaurantService.getRestaurant(1004L);
 		Restaurant restaurant = restaurantService.getRestaurant(1004L);
 
 		assertThat(restaurant.getId(), is(1004L));
@@ -41,13 +72,14 @@ class RestaurantServiceTest {
 
 	@Test
 	public void getRestaurants() {
-		restaurantRepository = new RestaurantRepositoryImpl();
-		menuItemRepository = new MenuItemRepositoryImpl();
-		restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
+//		restaurantRepository = new RestaurantRepositoryImpl();
+//		menuItemRepository = new MenuItemRepositoryImpl();
+//		restaurantService = new RestaurantService(restaurantRepository,menuItemRepository);
 
+		setUp();
 		List<Restaurant> restaurants = restaurantService.getRestaurants();
 		System.out.println(menuItemRepository.findAllByRestaurantId(1004L).get(0).getName());
-		
+
 		Restaurant restaurant = restaurants.get(0);
 		assertThat(restaurant.getId(), is(1004L));
 
--- src/test/java/com/yosep/restaurant/interfaces/RestaurantControllerTest.java
+++ src/test/java/com/yosep/restaurant/interfaces/RestaurantControllerTest.java
@@ -6,19 +6,26 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.yosep.restaurant.application.RestaurantService;
+import com.yosep.restaurant.domain.MenuItem;
 import com.yosep.restaurant.domain.MenuItemRepository;
 import com.yosep.restaurant.domain.MenuItemRepositoryImpl;
+import com.yosep.restaurant.domain.Restaurant;
 import com.yosep.restaurant.domain.RestaurantRepository;
 import com.yosep.restaurant.domain.RestaurantRepositoryImpl;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RestaurantController.class) // Restaurant 컨트롤러를 테스트하겠다는 어노테이션
@@ -30,17 +37,25 @@ public class RestaurantControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
-	@SpyBean(RestaurantRepositoryImpl.class)
-	private RestaurantRepository restaurantRepository;
-
-	@SpyBean(MenuItemRepositoryImpl.class)
-	private MenuItemRepository menuItemRepository;
-	
-	@SpyBean(RestaurantService.class)
+	@MockBean
 	private RestaurantService restaurantService;
+	
+//	@SpyBean(RestaurantRepositoryImpl.class)
+//	private RestaurantRepository restaurantRepository;
+//
+//	@SpyBean(MenuItemRepositoryImpl.class)
+//	private MenuItemRepository menuItemRepository;
+//	
+//	@SpyBean(RestaurantService.class)
+//	private RestaurantService restaurantService;
 	
 	@Test
 	public void list() throws Exception {
+		List<Restaurant> restaurants = new ArrayList<Restaurant>();
+		restaurants.add(new Restaurant(1004L, "Mcdonald", "Seoul"));
+		
+		given(restaurantService.getRestaurants()).willReturn(restaurants);
+		
 		mvc.perform(get("/restaurants")).andExpect(status().isOk())
 				.andExpect(content().string(containsString("\"id\":1004")))
 				.andExpect(content().string(containsString("\"name\":\"Mcdonald\"")))
@@ -49,11 +64,24 @@ public class RestaurantControllerTest {
 	
 	@Test
 	public void detail() throws Exception {
+		Restaurant restaurant1 = new Restaurant(1004L, "Joker House","Seoul");
+		restaurant1.addMenuItem(new MenuItem("BigMac"));
+		Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food","Seoul");
+		
+		given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+		given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+		
 		mvc.perform(get("/restaurants/1004"))
 			.andExpect(status().isOk())
 			.andExpect(content().string(containsString("\"id\":1004")))
-			.andExpect(content().string(containsString("\"name\":\"Mcdonald\"")))
+			.andExpect(content().string(containsString("\"name\":\"Joker House\"")))
 			.andExpect(content().string(containsString("\"address\":\"Seoul\"")))
 			.andExpect(content().string(containsString("BigMac")));
+	
+		mvc.perform(get("/restaurants/2020"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("\"id\":2020")))
+		.andExpect(content().string(containsString("\"name\":\"Cyber Food\"")))
+		.andExpect(content().string(containsString("\"address\":\"Seoul\"")));
 	}
 }
